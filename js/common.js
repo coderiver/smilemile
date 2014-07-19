d00124385f6b3483c69b4d7c983672e68b6add92
Valera19900 <kaktuz34@gmail.com>
2014-07-19 19:20:58 +0300

    index inner_translate page

--- js/common.js
+++ js/common.js
@@ -4,5 +4,60 @@ head.ready(function() {
 	// 	$(".js-popup").hide();
 	// });
 
-	console.log($('body').html());
+	//click
+	$(document).click(function() {
+        $(".js-select-list").hide();
+        $(".js-select").removeClass("is-active");
+    });
+
+    //selectList
+    function selectList() {
+        var select = $(".js-select");
+        var select_list = $(".js-select-list");
+        $("body").on("click", ".js-select", function(event){
+            if ($(this).hasClass("is-active")) {
+                select.removeClass("is-active");
+                select_list.hide();
+            }
+            else {
+                select.removeClass("is-active");
+                select_list.hide();
+                $(this).find(".js-select-list").show();
+                $(this).addClass("is-active");
+            }
+            event.stopPropagation();
+        });
+        $("body").on("click", ".js-select-list li", function(event){
+            var id = $(this).attr("data-id");
+            var text = $(this).text();
+            $(this).parents(".js-select").find(".js-select-text").text(text);
+            $(this).parents(".js-select").find(".js-select-input").val(id);
+            $(this).parent().hide();
+            $(this).parents(".js-select").removeClass("is-active");
+            event.stopPropagation();
+        });
+    }  
+    selectList();
+    $("body").on("click", ".js-select", function(event){
+        event.stopPropagation();
+    });
+
+    $('.js-log').click(function(){
+    	var js_form = $('.js-log-in-form');
+    	if ($(this).hasClass("is-active")) {
+    	    $(this).removeClass("is-active");
+    	    js_form.slideUp();
+    	}
+    	else {
+    	    $(this).removeClass("is-active");
+    	    js_form.slideUp();
+    	    js_form.slideDown();
+    	    $(this).addClass("is-active");
+    	}
+    });
+
 });
+	//scrollPane
+	$('.scroll-pane').jScrollPane({
+		hideFocus: true
+	});
--- js/init.js
+++ js/init.js
@@ -1,3 +1,5 @@
 head.load("js/lib/jquery.js", 
 					"js/lib/cycle.js",
+					"js/lib/jquery.mousewheel.js",
+					"js/lib/jquery.jscrollpane.js",
 					"js/common.js");
